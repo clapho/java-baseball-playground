fc881f7781d0fdc26915e75705d96aa01b5f4313
SoohoPark <clapp301@gmail.com>
2023-07-29 14:53:44 +0900

    학습테스트 - SetTest

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -21,7 +21,6 @@ public class SetTest {
     void setUp() {
         numbers = new HashSet<>();
         numbers.add(1);
-        numbers.add(1);
         numbers.add(2);
         numbers.add(3);
     }
